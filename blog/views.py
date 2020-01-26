7a91c3e081800405cd39be7adee2bd28fbc43447
santhosh <santhoshcusat17@gmail.com>
2020-01-26 20:06:28 +0530

    colors added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+import random
 # Create your views here.
 
 
 def home(request):
+    colors = ['#977', '#997797', '#778799', '#779991', '#859977', '#998a77']
+    request.session['color'] = random.choice(colors)
     return render(request, 'blog/home.html')
